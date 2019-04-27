1dbc2d40924a7b441eab3f237e92b8c9153704ed
Bruno Cavalcanti <bh-cavalcantii@hotmail.com>
2019-04-27 19:21:26 -0300

    setup php unit

--- tests/ChangePassword.php
+++ tests/ChangePassword.php
@@ -8,33 +8,39 @@ use Facebook\WebDriver\WebDriverBy;
 
 
 class ChangePasswordTests extends TestCase {
+    private $driver = null ;
+    
+    protected function setUp():void{
+
+        $this->driver = RemoteWebDriver::create('http://localhost:4444',DesiredCapabilities::chrome());
+        $this->driver->manage()->window()->maximize();
+        $this->driver->manage()->timeouts()->implicitlyWait(5);
+        $this->driver->get("http://www.juliodelima.com.br/taskit");
+        $this->driver->findElement(WebDriverBy::linkText('Sign in'))->click();
+        $this->driver->findElement(WebDriverBy::cssSelector('#signinbox input[name="login"]'))->sendKeys('bruno.prado');
+        $this->driver->findElement(WebDriverBy::cssSelector('#signinbox input[name="password"]'))->sendKeys('1234');
+        $this->driver->findElement(WebDriverBy::cssSelector('#signinbox'))->findElement(WebDriverBy::linkText('SIGN IN'))->click();   
+    }
+    protected function tearDown():void{
+        $this->driver->quit();
+    }
     public function testChangePassword() {
-    // Arrange
-    $driver = RemoteWebDriver::create('http://localhost:4444',DesiredCapabilities::chrome());
-    $driver->manage()->window()->maximize();
-    $driver->manage()->timeouts()->implicitlyWait(5);
-    $driver->get("http://www.juliodelima.com.br/taskit");
-
-    // Act
-    $driver->findElement(WebDriverBy::linkText('Sign in'))->click();
-    $driver->findElement(WebDriverBy::cssSelector('#signinbox input[name="login"]'))->sendKeys('bruno.prado');
-    $driver->findElement(WebDriverBy::cssSelector('#signinbox input[name="password"]'))->sendKeys('1234');
-    $driver->findElement(WebDriverBy::cssSelector('#signinbox'))->findElement(WebDriverBy::linkText('SIGN IN'))->click();
-    $driver->findElement(WebDriverBy::className('me'))->click();
-    $driver->findElement(WebDriverBy::cssSelector('a[href="#secret"]'))->click();
-    $driver->findElement(WebDriverBy::cssSelector('input[name="password"]'))->sendKeys('123456789');
-    $password = $driver->findElement(WebDriverBy::cssSelector('input[name="password"]'))->getAttribute('value');
-    $passwordSize = strlen($password);
+        // Arrange
+        $this->driver->findElement(WebDriverBy::className('me'))->click();
+        $this->driver->findElement(WebDriverBy::cssSelector('a[href="#secret"]'))->click();
+        $this->driver->findElement(WebDriverBy::cssSelector('input[name="password"]'))->sendKeys('123456789');
+        $password = $this->driver->findElement(WebDriverBy::cssSelector('input[name="password"]'))->getAttribute('value');
+        $passwordSize = strlen($password);
 
 
-    // Assert
-    $this->assertThat(
-        $passwordSize,
-        $this->logicalAnd(
-            $this->greaterThanOrEqual(8),
-            $this->lessThanOrEqual(12)
-        ),"Ola"
-    );
+        // Assert
+        $this->assertThat(
+            $passwordSize,
+            $this->logicalAnd(
+                $this->greaterThanOrEqual(8),
+                $this->lessThanOrEqual(12)
+            ),"Ola"
+        );
     }
    }
 
